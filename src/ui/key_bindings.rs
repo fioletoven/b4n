e46c3e6fa6bf07f97cf5cdac407f2550a51985f5
Piotr Frankowski <piotr.frankowski@vp.pl>
2025-08-15 08:46:13 +0200

    Add Serialize and Deserialize to KeyCombination

--- src/ui/key_bindings.rs
+++ src/ui/key_bindings.rs
@@ -1,10 +1,26 @@
 use crossterm::event::{KeyCode, KeyEvent, KeyModifiers};
+use serde::de::{self, Unexpected, Visitor};
+use serde::{Deserialize, Deserializer, Serialize, Serializer};
+use std::fmt::{self};
+use std::str::FromStr;
 
 #[cfg(test)]
 #[path = "./key_bindings.tests.rs"]
 mod key_bindings_tests;
 
-#[derive(Debug, PartialEq)]
+/// Possible errors from [`KeyCombination`] parsing.
+#[derive(thiserror::Error, Debug)]
+pub enum KeyCombinationError {
+    /// Unknown key modifier.
+    #[error("unknown key modifier")]
+    UnknownModifier,
+
+    /// Unknown key code.
+    #[error("unknown key code")]
+    UnknownCode,
+}
+
+#[derive(Debug, PartialEq, Eq, Hash)]
 pub struct KeyCombination {
     pub code: KeyCode,
     pub modifiers: KeyModifiers,
@@ -16,29 +32,34 @@ impl From<KeyEvent> for KeyCombination {
     }
 }
 
-impl From<&str> for KeyCombination {
-    fn from(value: &str) -> Self {
-        if value.chars().count() == 1 {
-            return KeyCombination::new(
+impl FromStr for KeyCombination {
+    type Err = KeyCombinationError;
+
+    fn from_str(value: &str) -> Result<Self, Self::Err> {
+        let len = value.chars().count();
+        if len == 0 {
+            return Err(KeyCombinationError::UnknownCode);
+        } else if len == 1 {
+            return Ok(KeyCombination::new(
                 KeyModifiers::NONE,
                 KeyCode::Char(value.chars().next().unwrap().to_ascii_lowercase()),
-            );
+            ));
+        } else if value.contains("++") {
+            return Err(KeyCombinationError::UnknownCode);
         }
 
         let plus = value.ends_with('+');
         let elements = value.split('+').filter(|s| !s.is_empty()).collect::<Vec<_>>();
 
-        if elements.is_empty() && plus {
-            KeyCombination::new(KeyModifiers::NONE, KeyCode::Char('+'))
-        } else if elements.is_empty() {
-            KeyCombination::new(KeyModifiers::NONE, KeyCode::Null)
+        if elements.is_empty() {
+            Err(KeyCombinationError::UnknownModifier)
         } else if elements.len() == 1 && plus {
-            KeyCombination::from(&[elements[0]], "+")
+            Err(KeyCombinationError::UnknownCode)
         } else if elements.len() == 1 {
-            KeyCombination::from(&[], elements[0])
+            Ok(KeyCombination::try_from(&[], elements[0])?)
         } else {
             let len = elements.len().saturating_sub(1);
-            KeyCombination::from(&elements[..len], elements[len])
+            Ok(KeyCombination::try_from(&elements[..len], elements[len])?)
         }
     }
 }
@@ -46,7 +67,7 @@ impl From<&str> for KeyCombination {
 impl KeyCombination {
     pub fn new(modifiers: KeyModifiers, code: KeyCode) -> Self {
         let code = if let KeyCode::Char(c) = code {
-            KeyCode::Char(c.to_ascii_lowercase())
+            KeyCode::Char(c.to_ascii_uppercase())
         } else {
             code
         };
@@ -54,52 +75,130 @@ impl KeyCombination {
         Self { code, modifiers }
     }
 
-    pub fn from(modifiers: &[&str], code: &str) -> Self {
+    pub fn try_from(modifiers: &[&str], code: &str) -> Result<Self, KeyCombinationError> {
         let code = match code.chars().count() {
             0 => KeyCode::Null,
-            1 => KeyCode::Char(code.chars().next().unwrap().to_ascii_lowercase()),
-            _ => get_code_from_name(code),
+            1 => KeyCode::Char(code.chars().next().unwrap().to_ascii_uppercase()),
+            _ => get_code_from_name(code)?,
         };
 
         let mut all_modifiers = KeyModifiers::NONE;
         for modifier in modifiers {
-            all_modifiers |= get_modifier_from_name(modifier);
+            all_modifiers |= get_modifier_from_name(modifier)?;
         }
 
-        Self {
+        Ok(Self {
             code,
             modifiers: all_modifiers,
+        })
+    }
+}
+
+impl Serialize for KeyCombination {
+    fn serialize<S: Serializer>(&self, serializer: S) -> Result<S::Ok, S::Error> {
+        let modifiers = get_string_from_modifiers(&self.modifiers);
+        if modifiers.is_empty() {
+            serializer.serialize_str(&self.code.to_string())
+        } else {
+            serializer.serialize_str(&format!("{}+{}", modifiers, self.code))
         }
     }
 }
 
-fn get_modifier_from_name(modifier: &str) -> KeyModifiers {
-    match modifier.to_ascii_lowercase().as_str() {
-        "shift" => KeyModifiers::SHIFT,
-        "alt" => KeyModifiers::ALT,
-        "ctrl" => KeyModifiers::CONTROL,
-        "control" => KeyModifiers::CONTROL,
-        _ => KeyModifiers::NONE,
+impl<'de> Deserialize<'de> for KeyCombination {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        deserializer.deserialize_str(KeyCombinationVisitor)
     }
 }
 
-fn get_code_from_name(code: &str) -> KeyCode {
-    match code.to_ascii_lowercase().as_str() {
-        "backspace" => KeyCode::Backspace,
-        "enter" => KeyCode::Enter,
-        "left" => KeyCode::Left,
-        "right" => KeyCode::Right,
-        "up" => KeyCode::Up,
-        "down" => KeyCode::Down,
-        "home" => KeyCode::Home,
-        "end" => KeyCode::End,
-        "pageup" => KeyCode::PageUp,
-        "pagedown" => KeyCode::PageDown,
-        "tab" => KeyCode::Tab,
-        "backtab" => KeyCode::BackTab,
-        "delete" => KeyCode::Delete,
-        "insert" => KeyCode::Insert,
-        "esc" => KeyCode::Esc,
-        _ => KeyCode::Null,
+/// Internal [`Visitor`] for deserializing [`KeyCombination`].
+struct KeyCombinationVisitor;
+
+impl Visitor<'_> for KeyCombinationVisitor {
+    type Value = KeyCombination;
+
+    fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+        formatter.write_str("a string containing key combination")
+    }
+
+    fn visit_str<E>(self, value: &str) -> Result<KeyCombination, E>
+    where
+        E: de::Error,
+    {
+        match KeyCombination::from_str(value) {
+            Ok(key) => Ok(key),
+            Err(_) => Err(de::Error::invalid_value(Unexpected::Str(value), &self)),
+        }
+    }
+}
+
+fn get_string_from_modifiers(modifiers: &KeyModifiers) -> String {
+    let mut result = String::new();
+    let mut first = true;
+
+    for modifier in modifiers.iter() {
+        if !first {
+            result.push('+');
+        }
+
+        first = false;
+        match modifier {
+            KeyModifiers::SHIFT => result.push_str("Shift"),
+            KeyModifiers::ALT => result.push_str("Alt"),
+            KeyModifiers::CONTROL => result.push_str("Ctrl"),
+            _ => (),
+        };
+    }
+
+    result
+}
+
+fn get_modifier_from_name(modifier: &str) -> Result<KeyModifiers, KeyCombinationError> {
+    match modifier.to_ascii_lowercase().as_str() {
+        "shift" => Ok(KeyModifiers::SHIFT),
+        "alt" => Ok(KeyModifiers::ALT),
+        "option" => Ok(KeyModifiers::ALT),
+        "ctrl" => Ok(KeyModifiers::CONTROL),
+        "control" => Ok(KeyModifiers::CONTROL),
+        _ => Err(KeyCombinationError::UnknownModifier),
+    }
+}
+
+fn get_code_from_name(code: &str) -> Result<KeyCode, KeyCombinationError> {
+    let code = code.to_ascii_lowercase();
+    let code = code.as_str();
+    if code.len() >= 2
+        && code.len() <= 3
+        && code.starts_with('f')
+        && let Ok(num) = code[1..].parse()
+    {
+        if num > 0 && num <= 12 {
+            return Ok(KeyCode::F(num));
+        } else {
+            return Err(KeyCombinationError::UnknownCode);
+        }
+    }
+
+    match code {
+        "backspace" => Ok(KeyCode::Backspace),
+        "enter" => Ok(KeyCode::Enter),
+        "left" => Ok(KeyCode::Left),
+        "right" => Ok(KeyCode::Right),
+        "up" => Ok(KeyCode::Up),
+        "down" => Ok(KeyCode::Down),
+        "home" => Ok(KeyCode::Home),
+        "end" => Ok(KeyCode::End),
+        "pageup" => Ok(KeyCode::PageUp),
+        "pagedown" => Ok(KeyCode::PageDown),
+        "tab" => Ok(KeyCode::Tab),
+        "backtab" => Ok(KeyCode::BackTab),
+        "delete" => Ok(KeyCode::Delete),
+        "insert" => Ok(KeyCode::Insert),
+        "esc" => Ok(KeyCode::Esc),
+        "null" => Ok(KeyCode::Null),
+        _ => Err(KeyCombinationError::UnknownCode),
     }
 }
--- src/ui/key_bindings.tests.rs
+++ src/ui/key_bindings.tests.rs
@@ -2,33 +2,96 @@ use super::*;
 
 #[test]
 fn from_str_test() {
-    assert_eq!(KeyCombination::new(KeyModifiers::NONE, KeyCode::Char('+')), "+".into());
-    assert_eq!(KeyCombination::new(KeyModifiers::NONE, KeyCode::Char('+')), "++".into());
-    assert_eq!(KeyCombination::new(KeyModifiers::CONTROL, KeyCode::Char('+')), "Ctrl+".into());
-    assert_eq!(KeyCombination::new(KeyModifiers::ALT, KeyCode::Char('+')), "Alt++".into());
-    assert_eq!(KeyCombination::new(KeyModifiers::ALT, KeyCode::Char('+')), "Alt++++".into());
-    assert_eq!(KeyCombination::new(KeyModifiers::NONE, KeyCode::Char('+')), "unknown+".into());
-
-    assert_eq!(KeyCombination::new(KeyModifiers::ALT, KeyCode::Char('d')), "Alt+D".into());
-    assert_eq!(KeyCombination::new(KeyModifiers::SHIFT, KeyCode::Char('e')), "SHIFT+e".into());
-    assert_eq!(KeyCombination::new(KeyModifiers::SHIFT, KeyCode::Char('?')), "shift+?".into());
+    assert!(KeyCombination::from_str("").is_err());
+    assert!(KeyCombination::from_str("++").is_err());
+    assert!(KeyCombination::from_str("++++").is_err());
+    assert!(KeyCombination::from_str("Ctrl").is_err());
+    assert!(KeyCombination::from_str("Ctrl+").is_err());
+    assert!(KeyCombination::from_str("Alt++").is_err());
+    assert!(KeyCombination::from_str("Alt+++++").is_err());
+    assert!(KeyCombination::from_str("unknown+").is_err());
+    assert!(KeyCombination::from_str("unknown+aa").is_err());
+    assert!(KeyCombination::from_str("unknown+z").is_err());
 
     assert_eq!(
-        KeyCombination::new(KeyModifiers::ALT | KeyModifiers::SHIFT, KeyCode::Char('w')),
-        "shift+ALT+W".into()
+        KeyCombination::new(KeyModifiers::NONE, KeyCode::Char('+')),
+        KeyCombination::from_str("+").unwrap()
     );
-
-    assert_eq!(KeyCombination::new(KeyModifiers::NONE, KeyCode::Null), "".into());
-    assert_eq!(KeyCombination::new(KeyModifiers::NONE, KeyCode::Null), "unknown+aa".into());
     assert_eq!(
-        KeyCombination::new(KeyModifiers::NONE, KeyCode::Char('z')),
-        "unknown+z".into()
+        KeyCombination::new(KeyModifiers::ALT, KeyCode::Char('D')),
+        KeyCombination::from_str("Alt+D").unwrap()
     );
-
-    assert_eq!(KeyCombination::new(KeyModifiers::ALT, KeyCode::Home), "alt+home".into());
-    assert_eq!(KeyCombination::new(KeyModifiers::CONTROL, KeyCode::Up), "control+up".into());
+    assert_eq!(
+        KeyCombination::new(KeyModifiers::SHIFT, KeyCode::Char('E')),
+        KeyCombination::from_str("SHIFT+e").unwrap()
+    );
+    assert_eq!(
+        KeyCombination::new(KeyModifiers::SHIFT, KeyCode::Char('?')),
+        KeyCombination::from_str("shift+?").unwrap()
+    );
+    assert_eq!(
+        KeyCombination::new(KeyModifiers::ALT | KeyModifiers::SHIFT, KeyCode::Char('W')),
+        KeyCombination::from_str("shift+ALT+W").unwrap()
+    );
+    assert_eq!(
+        KeyCombination::new(KeyModifiers::ALT, KeyCode::Home),
+        KeyCombination::from_str("alt+home").unwrap()
+    );
+    assert_eq!(
+        KeyCombination::new(KeyModifiers::CONTROL, KeyCode::Up),
+        KeyCombination::from_str("control+up").unwrap()
+    );
     assert_eq!(
         KeyCombination::new(KeyModifiers::ALT | KeyModifiers::CONTROL | KeyModifiers::SHIFT, KeyCode::Left),
-        "alt+Shift+control+LEFT".into()
+        KeyCombination::from_str("option+Shift+control+LEFT").unwrap()
     );
+}
+
+#[test]
+fn serialize_test() {
+    let key = serde_yaml::to_string(&KeyCombination::new(KeyModifiers::NONE, KeyCode::Null)).unwrap();
+    assert_eq!("'Null'", key.trim());
+
+    let key = serde_yaml::to_string(&KeyCombination::new(KeyModifiers::NONE, KeyCode::Char('a'))).unwrap();
+    assert_eq!("A", key.trim());
+
+    let key = serde_yaml::to_string(&KeyCombination::new(KeyModifiers::NONE, KeyCode::F(5))).unwrap();
+    assert_eq!("F5", key.trim());
+
+    let key = serde_yaml::to_string(&KeyCombination::new(KeyModifiers::SHIFT, KeyCode::Char('A'))).unwrap();
+    assert_eq!("Shift+A", key.trim());
+
+    let key = serde_yaml::to_string(&KeyCombination::new(
+        KeyModifiers::CONTROL | KeyModifiers::SHIFT | KeyModifiers::ALT,
+        KeyCode::Char('z'),
+    ))
+    .unwrap();
+    assert_eq!("Shift+Ctrl+Alt+Z", key.trim());
+
+    let key = serde_yaml::to_string(&KeyCombination::new(KeyModifiers::SHIFT, KeyCode::Backspace)).unwrap();
+    assert_eq!("Shift+Backspace", key.trim());
+}
+
+#[test]
+fn deserialize_test() {
+    let key = serde_yaml::from_str("'Null'").unwrap();
+    assert_eq!(KeyCombination::new(KeyModifiers::NONE, KeyCode::Null), key);
+
+    let key = serde_yaml::from_str("Ctrl+A").unwrap();
+    assert_eq!(KeyCombination::new(KeyModifiers::CONTROL, KeyCode::Char('A')), key);
+
+    let key = serde_yaml::from_str("shift+Ctrl+x").unwrap();
+    assert_eq!(
+        KeyCombination::new(KeyModifiers::CONTROL | KeyModifiers::SHIFT, KeyCode::Char('X')),
+        key
+    );
+
+    let key = serde_yaml::from_str("LEFT").unwrap();
+    assert_eq!(KeyCombination::new(KeyModifiers::NONE, KeyCode::Left), key);
+
+    let key = serde_yaml::from_str("F7").unwrap();
+    assert_eq!(KeyCombination::new(KeyModifiers::NONE, KeyCode::F(7)), key);
+
+    let key = serde_yaml::from_str("Shift+F12").unwrap();
+    assert_eq!(KeyCombination::new(KeyModifiers::SHIFT, KeyCode::F(12)), key);
 }
