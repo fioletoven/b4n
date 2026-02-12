4fe4ae89a0f7f7ba9f1a3933dfb19e2379198103
Piotr Frankowski <piotr.frankowski@vp.pl>
2026-02-12 21:23:56 +0100

    Fix container name in header

--- b4n/ui/views/logs/view.rs
+++ b4n/ui/views/logs/view.rs
@@ -1,7 +1,7 @@
 use b4n_common::{DEFAULT_MESSAGE_DURATION, IconKind, NotificationSink};
 use b4n_config::keys::KeyCommand;
 use b4n_kube::client::KubernetesClient;
-use b4n_kube::{PODS, ContainerRef};
+use b4n_kube::{ContainerRef, PODS};
 use b4n_tui::widgets::{ActionItem, ActionsListBuilder};
 use b4n_tui::{MouseEventKind, ResponseEvent, Responsive, TuiEvent};
 use crossterm::event::KeyCode;
@@ -59,7 +59,8 @@ impl LogsView {
         let select = app_data.borrow().theme.colors.syntax.logs.select;
         let search = app_data.borrow().theme.colors.syntax.logs.search;
         let area = ContentViewer::<LogsContent>::get_content_area(workspace);
-        let container = (containers.len() == 1).then(|| containers[0].container.clone()).flatten();
+        let non_init = containers.iter().filter(|c| !c.is_init).collect::<Vec<_>>();
+        let container = (non_init.len() == 1).then(|| non_init[0].container.clone()).flatten();
         let logs = ContentViewer::new(Rc::clone(&app_data), select, search, area).with_header(
             if previous { "previous logs" } else { "logs" },
             '',
